7683a08c173f5dfde716504fe753f3a50eff9bb3
Jury Soldatenkov <yurysold@googlemail.com>
2015-02-19 23:19:47 +0300

    Initial commit

---
+++ src/Tests/ScratchPad.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Codestellation.Pulsar.Tests
+{
+    [TestFixture]
+    public class ScratchPad
+    {
+        [Test]
+        public void Run_my_thought()
+        {
+            
+        }
+    }
+}
