9974de95ec1afd183d892ecab2131334ffcd377c
Jury Soldatenkov <yurysold@googlemail.com>
2015-08-28 23:32:03 +0300

    Simple task divided into abstract and action task (close #10)

---
+++ src/Pulsar/Tasks/AbstractTask.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codestellation.Pulsar.Tasks
+{
+    /// <summary>
+    /// Scaffolder to build custom <see cref="ITask"/> implementations
+    /// </summary>
+    public abstract class AbstractTask : ITask
+    {
+        private readonly HashSet<ITrigger> _triggers;
+
+        /// <summary>
+        /// Unique identifier of task used by <see cref="IScheduler"/> to distinguish task.
+        /// </summary>
+        public virtual Guid Id { get; protected set; }
+
+        /// <summary>
+        /// Human readable task name.
+        /// </summary>
+        public virtual string Title { get; protected set; }
+
+        /// <summary>
+        /// Collection of <see cref="ITrigger"/> that fire  start task.
+        /// </summary>
+        public IEnumerable<ITrigger> Triggers => _triggers;
+
+        /// <summary>
+        /// Instantiates new instace of <see cref="AbstractTask"/>
+        /// </summary>
+        protected AbstractTask()
+        {
+            _triggers = new HashSet<ITrigger>();
+        }
+
+        /// <summary>
+        /// Addes trigger to the <see cref="AbstractTask"/>
+        /// </summary>
+        /// <param name="trigger"></param>
+        public void AddTrigger(ITrigger trigger)
+        {
+            if (trigger == null)
+            {
+                throw new ArgumentNullException(nameof(trigger));
+            }
+            OnTriggerAdded(trigger);
+        }
+
+        /// <summary>
+        /// Allows to intercept trigger attach process.
+        /// </summary>
+        /// <param name="trigger"></param>
+        protected virtual void OnTriggerAdded(ITrigger trigger)
+        {
+            _triggers.Add(trigger);
+        }
+
+        /// <summary>
+        /// Removes trigger from the <see cref="AbstractTask"/>.
+        /// </summary>
+        /// <param name="trigger"></param>
+        public void RemoveTrigger(ITrigger trigger)
+        {
+            if (trigger == null)
+            {
+                throw new ArgumentNullException(nameof(trigger));
+            }
+
+            OnTriggerRemoved(trigger);
+        }
+
+        /// <summary>
+        /// Allows to intercept trigger removal process.
+        /// </summary>
+        /// <param name="trigger"><see cref="ITrigger"/> to remove</param>
+        protected virtual void OnTriggerRemoved(ITrigger trigger)
+        {
+            if (!_triggers.Remove(trigger))
+            {
+                return;
+            }
+            trigger.Stop();
+            (trigger as IDisposable)?.Dispose();
+        }
+
+        /// <summary>
+        /// Called by <see cref="IScheduler"/> when one of the task triggers fire.
+        /// </summary>
+        public abstract void Run();
+    }
+}
---
+++ src/Pulsar/Tasks/ActionTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Codestellation.Pulsar.Tasks
+{
+    /// <summary>
+    /// Simple task that utilize <see cref="Action"/>
+    /// </summary>
+    public class ActionTask : AbstractTask
+    {
+        private readonly Action _action;
+
+        /// <summary>
+        /// Initialize new instance of <see cref="ActionTask"/> class
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="action"></param>
+        public ActionTask(Action action, string name = null)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+            _action = action;
+            Id = Guid.NewGuid();
+            Title = string.IsNullOrWhiteSpace(name) ? $"Task {Id}" : name;
+        }
+
+        /// <summary>
+        /// Called by <see cref="IScheduler"/> when one of the task triggers fire.
+        /// </summary>
+        public override void Run()
+        {
+            _action();
+        }
+    }
+}
