0495dc4199d4e4fefa56b839b913906e10eb9b5e
Jury Soldatenkov <yurysold@googlemail.com>
2015-09-04 22:26:44 +0300

    Integer index made internal

--- src/Pulsar/Cron/IntegerIndex.cs
+++ src/Pulsar/Cron/IntegerIndex.cs
@@ -1,24 +1,40 @@
-﻿namespace Codestellation.Pulsar.Cron
+﻿using System.Runtime.CompilerServices;
+
+namespace Codestellation.Pulsar.Cron
 {
-    public struct IntegerIndex
+    internal struct IntegerIndex
     {
         public const sbyte NotFound = -1;
 
         private readonly sbyte[] _indexArray;
+
         public IntegerIndex(SimpleCronField field)
         {
             _indexArray = new sbyte[field.Settings.MaxValue + 1];
 
+            FillDefaults();
+            FillKnownValues(field);
+            FillRedirectionValues();
+        }
+
+        private void FillDefaults()
+        {
             for (int i = 0; i < _indexArray.Length; i++)
             {
                 _indexArray[i] = NotFound;
             }
+        }
 
+        private void FillKnownValues(SimpleCronField field)
+        {
             foreach (var value in field.Values)
             {
                 _indexArray[value] = (sbyte)value;
             }
+        }
 
+        private void FillRedirectionValues()
+        {
             int next = NotFound;
             for (int i = _indexArray.Length - 1; i >= 0; i--)
             {
@@ -32,6 +48,7 @@
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int GetValue(int value)
         {
             return _indexArray[value];
