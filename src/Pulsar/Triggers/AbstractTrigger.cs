1193d330f75c25b44aa6f745325922bf3d86d0e4
Jury Soldatenkov <yurysold@googlemail.com>
2015-09-05 22:12:09 +0300

    Documented abstract trigger

--- src/Pulsar/Triggers/AbstractTrigger.cs
+++ src/Pulsar/Triggers/AbstractTrigger.cs
@@ -2,11 +2,18 @@
 
 namespace Codestellation.Pulsar.Triggers
 {
+    /// <summary>
+    /// Base class for building <see cref="ITrigger"/> implmementations
+    /// </summary>
     public abstract class AbstractTrigger : ITrigger
     {
         private TriggerCallback _callback;
         private bool _started;
 
+        /// <summary>
+        ///  Forces trigger to callback at specified schedule
+        /// </summary>
+        /// <param name="callback"></param>
         public void Start(TriggerCallback callback)
         {
             if (callback == null)
@@ -19,19 +26,31 @@ namespace Codestellation.Pulsar.Triggers
             _started = true;
         }
 
+        /// <summary>
+        /// Called from <see cref="Start"/> when parameter validation succeed
+        /// </summary>
         protected virtual void OnStart()
         {
         }
 
+        /// <summary>
+        ///  Stops trigger
+        /// </summary>
         public void Stop()
         {
             OnStop();
         }
 
+        /// <summary>
+        /// Called when <see cref="Stop"/> is called
+        /// </summary>
         protected virtual void OnStop()
         {
         }
 
+        /// <summary>
+        /// Invokes callback supplied in <see cref="Start"/> method if trigger is started
+        /// </summary>
         protected void InvokeCallback()
         {
             if (_started)
