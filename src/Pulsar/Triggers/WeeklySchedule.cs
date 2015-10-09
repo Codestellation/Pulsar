70a527c87a9511dc896ed352f93f6a59b23c9c79
Jury Soldatenkov <yurysold@gmail.com>
2015-10-09 15:28:32 +0300

    Introduced weekly schedule (close #19)

---
+++ src/Pulsar/Triggers/WeeklySchedule.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Codestellation.Pulsar.Misc;
+
+namespace Codestellation.Pulsar.Triggers
+{
+    public class WeeklySchedule : ISchedule
+    {
+        private readonly TimeZoneInfo _timeZone;
+        private readonly SortedSet<DayOfWeek> _days;
+        private readonly SortedSet<TimeSpan> _times;
+
+        public WeeklySchedule(IEnumerable<DayOfWeek> days, IEnumerable<TimeSpan> times, TimeZoneInfo timeZone)
+        {
+            _timeZone = timeZone;
+            if (days == null)
+            {
+                throw new ArgumentNullException(nameof(days));
+            }
+            if (times == null)
+            {
+                throw new ArgumentNullException(nameof(times));
+            }
+
+            if (timeZone == null)
+            {
+                throw new ArgumentNullException(nameof(timeZone));
+            }
+
+            _days = new SortedSet<DayOfWeek>(days);
+            _times = new SortedSet<TimeSpan>(times);
+
+            foreach (var time in times)
+            {
+                if (TimeSpan.FromDays(1) <= time)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(times), $"All element must be lower than {TimeSpan.FromDays(1)}, but was {time}");
+                }
+            }
+        }
+
+        public DateTime? NextAt
+        {
+            get
+            {
+                var now = Clock.UtcNow;
+                var day = now.DayOfWeek;
+                var time = now.TimeOfDay;
+                DateTime today = now.Date;
+
+                if (_days.Contains(day))
+                {
+                    foreach (var candidate in _times)
+                    {
+                        if (time <= candidate)
+                        {
+                            return today.Add(candidate);
+                        }
+                    }
+                }
+
+                for (int offsetDays = 1; offsetDays < 8; offsetDays++)
+                {
+                    var nextDay = today.AddDays(offsetDays);
+                    if (_days.Contains(nextDay.DayOfWeek))
+                    {
+                        return nextDay.Add(_times.Min);
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        public override string ToString()
+        {
+            var times = string.Join(",", _times);
+            var days = string.Join(",", _days);
+
+            return $"{times} {days} {_timeZone.Id}";
+        }
+    }
+}
---
+++ src/Tests/Triggers/WeeklyScheduleTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Codestellation.Pulsar.Misc;
+using Codestellation.Pulsar.Triggers;
+using NUnit.Framework;
+
+namespace Codestellation.Pulsar.Tests.Triggers
+{
+    [TestFixture]
+    public class WeeklyScheduleTests
+    {
+        [TestCase("2016-03-03 03:05:43Z", "2016-03-04 20:00:00Z", "Should be a day in the same week")]
+        [TestCase("2016-03-04 03:05:43Z", "2016-03-04 20:00:00Z", "Should be the same day")]
+        [TestCase("2016-03-05 03:05:43Z", "2016-03-11 20:00:00Z", "Should be a day at the next week")]
+        [TestCase("2016-03-04 23:05:43Z", "2016-03-11 20:00:00Z", "Should be a day at the next week")]
+        public void Should_calculate_next_occurrence(string nowString, string expectedString, string message)
+        {
+            //given
+            var now = DateTime.Parse(nowString).ToUniversalTime();
+            Clock.UtcNowFunction = () => now;
+            var days = new[] { DayOfWeek.Friday };
+            var times = new[] { TimeSpan.FromHours(20) };
+            var schedule = new WeeklySchedule(days, times, TimeZoneInfo.Local);
+            Console.WriteLine(schedule);
+
+            //when
+            var next = schedule.NextAt;
+
+            //then
+            var expected = DateTime.Parse(expectedString).ToUniversalTime();
+
+            DateTime dateTime = next.Value;
+            Assert.That(dateTime, Is.EqualTo(expected), message);
+            Assert.That(dateTime.Kind, Is.EqualTo(DateTimeKind.Utc));
+        }
+
+        [TearDown]
+        public void RepairClock()
+        {
+            Clock.UseDefault();
+        }
+    }
+}
